a08e17df924c91aa77a71451b1fd9417cbb54c8a
sunquan <sunquana@gmail.com>
2021-05-26 23:51:15 +0800

    add test

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
     println!("Hello, world!");
+
+    let a = 123;
+    let a = 456;
+    test_string();
+
+    add(1, 2);
+}
+
+// 测试字符串
+fn test_string() {
+    let mut s = "123";
+    println!("{}, len = {}", s, s.len())
+    // s = s.len();
+}
+
+/// Adds one to the number given.
+///
+/// # Examples
+///
+/// ```
+/// let x = add(1, 2);
+///
+/// ```
+
+fn add(a: i32, b: i32) -> i32 {
+    return a + b;
 }
