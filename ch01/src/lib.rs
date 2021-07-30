11187c440c8b7204b335f065e45952ba02980b90
sunquana <sunquana@gmail.com>
2021-07-30 16:34:27 +0800

    test: add fib test

--- ch01/src/lib.rs
+++ ch01/src/lib.rs
@@ -5,3 +5,8 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
 }
+
+#[test]
+fn test_panic() {
+    panic!("I am panic {}", 10);
+}
--- ch02_loop/src/lib.rs
+++ ch02_loop/src/lib.rs
@@ -3,6 +3,26 @@ fn test_loop() {
     // 测试循环
     let list = vec![10, 20, 30, 40];
     for one in list {
-        println!("{:?}", one);
+        println!("list = {:?}", one);
     }
+
+    for number in (1..4).rev() {
+        println!("rev = {}", number);
+    }
+
+    let mut index = 0;
+    while index < 3 {
+        println!("index =  {}", index);
+        index += 1;
+    }
+
+    println!("fib(5) = {}", fib(5));
+}
+
+// 1,1,2,3,5
+fn fib(x: i32) -> i32 {
+    if x < 2 {
+        return x;
+    }
+    return fib(x - 1) + fib(x - 2);
 }
